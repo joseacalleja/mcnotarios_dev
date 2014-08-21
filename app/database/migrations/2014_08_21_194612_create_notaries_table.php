485fdae875984c3c90df2f34e3ff6a4135ba836c
jose a calleja r <jose@ecotintas.mx>
2014-08-21 18:11:23 -0500

    incluye notaries table

---
+++ app/database/migrations/2014_08_21_194612_create_notaries_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotariesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Notaries table
+		Schema::create('notaries', function($table)
+		{
+			// unique
+			$table->increments('id');
+			$table->string('number');
+			$table->string('description');
+			$table->integer('responsible')->unsigned();
+			$table->string('cell_phone');
+			$table->string('office_phone');
+			$table->string('email');
+			// unique
+			$table->string('curp')->unique();
+			// unique
+			$table->string('rfc')->unique();
+			$table->string('legal_name');
+			$table->string('street');
+			$table->string('int_number');
+			$table->string('ext_number')->nullable();
+			$table->string('colony');
+			$table->string('add_ubication')->nullable();
+			$table->string('city');
+			$table->string('state');
+			$table->string('country');
+			$table->string('zip_code');
+			$table->timestamps();
+			$table->foreign('responsible')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('notaries');
+	}
+
+}
